d3711c0aa78adb5304b25676812e02551987c516
Shadow1088 <145645832+Shadow1088@users.noreply.github.com>
2023-09-21 10:44:46 +0200

    Added comments to num to binary code (python6.py)

--- python6.py
+++ python6.py
@@ -1,19 +1,19 @@
-list1 = [2600,1455,126,1568]
+list1 = [2600,1455,126,1568]  #numbers you want in binary code
 dict1 = {}
-start_num = 0
+start_num = 0 #key in dict1
 
 for thing in list1:
     current_01 = []
     while thing>1:
-        cele_cislo = thing//2
-        zbytek = thing%2       
-        current_01.append(zbytek)
-        thing = cele_cislo
+        cele_cislo = thing//2  #while the number is not below 1 it divides the number
+        zbytek = thing%2       # if the number is not odd, here is remainder found
+        current_01.append(zbytek) #the remainder is appended to the current_01 which is later added whole to the dictionary
+        thing = cele_cislo #the thing is number
 
 
-    current_01.append(1)
-    dict1[start_num] = list(reversed(current_01))
-    start_num = start_num + 1    
+    current_01.append(1) # this basically makes the while loop work like: thing>=1
+    dict1[start_num] = list(reversed(current_01)) 
+    start_num = start_num + 1  #updates the key in dict1
 
 for thing in dict1.items():
     print(thing)
